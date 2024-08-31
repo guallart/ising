5d1b3e51c1730f30002daa757aae7d7004162bdf
javier78gh <49785120+javier78gh@users.noreply.github.com>
2024-08-31 09:43:28 +0200

    Add files via upload

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+pub const L: usize = 12;
+pub const J: f64 = 1.0;
+pub const K_B: f64 = 1.0;
+pub const T_INI: f64 = 0.5;
+pub const T_FIN: f64 = 5.0;
+pub const N_TEMP: usize = 251;
+pub const N_ITERS: usize = 100_000;
